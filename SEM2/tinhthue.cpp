7a517025b10038c523b1638ea9a6f93d5ac10feb
addd28 <aduc92204@gmail.com>
2025-08-06 18:01:10 +0700

    Add files via upload

--- SEM2/tinhthue.cpp
+++ SEM2/tinhthue.cpp
@@ -5,7 +5,7 @@ int main(){
     scanf("%f", &L);
 
     if (L <= 11) {
-        printf("Ban khong duoc dong thue");
+        printf("Ban khong duoc dong T");
     } else {
         x = L - 11;
         if (x <= 5)
@@ -14,10 +14,15 @@ int main(){
             T = 5 * 0.05 + (x - 5) * 0.10;
         else if (x <= 18)
             T = 5 * 0.05 + 5 * 0.10 + (x - 10) * 0.15;
-        else
+        else if (x <= 32)
             T = 5 * 0.05 + 5 * 0.10 + 8 * 0.15 + (x - 18) * 0.20;
-
-        printf("Thue phai nop: %f trieu VND", T);
+        else if (x <= 52)
+            T = 5 * 0.05 + 5 * 0.10 + 8 * 0.15 + 14 * 0.20 + (x - 32) * 0.25;
+        else if (x <= 80)
+            T = 5 * 0.05 + 5 * 0.10 + 8 * 0.15 + 14 * 0.20 + 20 * 0.25 + (x - 52) * 0.30;
+        else
+            T = 5 * 0.05 + 5 * 0.10 + 8 * 0.15 + 14 * 0.20 + 20 * 0.25 + 28 * 0.30 + (x - 80) * 0.35;
+        printf("T phai nop: %f trieu VND", T);
     }
 }
 
